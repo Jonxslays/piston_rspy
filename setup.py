29fb361a93635d3f7ccded88cf5ecd5547dbd131
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-12-09 11:52:52 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as r:
 
 setup(
     name="piston_rspy",
-    version="0.1.3",
+    version="0.2.0",
     description="Python bindings for piston_rs.",
     author="Jonxslays",
     long_description=long_description,
